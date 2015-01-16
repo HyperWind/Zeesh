b3323aeae8869c7f572058df9fdcc482d54ce05b
Karolis Lasys (aka. HyperWind) <karolioofficial@outlook.com>
2015-01-17 01:17:01 +0200

    made the coding style more concrete

--- bundler.zsh
+++ bundler.zsh
@@ -3,67 +3,66 @@
 # loads themes
 
 load_themes() {
-        zeesh_debug "setting theme"
+	zeesh_debug "setting theme"
 
-        if [[ $1 == "random" ]]; then
-                zeesh_debug "using random theme"
-                local themes=($BUNDLE/themes/*.zsh-theme)
-                local n=${#themes[@]}
-                ((n=(RANDOM%n)+1))
-                local rand_theme=$themes[$n]
-                source $rand_theme
-                zeesh_message "$BUNDLE:t - loaded random theme $rand_theme"
-        else
-                if [[ ! -z $1 ]] && [[ -e "$BUNDLE/themes/$1.zsh-theme" ]]; then
-                        zeesh_debug "file $1.zsh-theme found"
-                        source "$BUNDLE/themes/$1.zsh-theme"
-                        zeesh_message "$BUNDLE:t - loaded theme $1"
-                else
-                        zeesh_warning "$BUNDLE:t - theme $1 not found, using default values"
-                fi
-        fi
+	if [[ $1 == "random" ]]; then
+		zeesh_debug "using random theme"
+		local themes=($BUNDLE/themes/*.zsh-theme)
+		local n=${#themes[@]}
+		((n=(RANDOM%n)+1))
+		local rand_theme=$themes[$n]
+		source $rand_theme
+		zeesh_message "$BUNDLE:t - loaded random theme $rand_theme"
+	else
+		if [[ ! -z $1 ]] && [[ -e "$BUNDLE/themes/$1.zsh-theme" ]]; then
+			zeesh_debug "file $1.zsh-theme found"
+			source "$BUNDLE/themes/$1.zsh-theme"
+			zeesh_message "$BUNDLE:t - loaded theme $1"
+		else
+			zeesh_warning "$BUNDLE:t - theme $1 not found, using default values"
+		fi
+	fi
 }
 
 # loads plugins
 
 load_plugins() {
-        zeesh_debug "loading plugins"
-        plugins=("${(ps: :)${1}}") 
-        
-        if [[ ${#plugins[@]} > 0 ]]; then
-                for plugin in $plugins; do
-                        zeesh_debug "loading plugin $plugin"
-                        if [[ -f "$BUNDLE/plugins/$plugin/$plugin.plugin.zsh" || -f "$BUNDLE/plugins/$plugin/_$plugin" ]]; then
-                                fpath=($BUNDLE/plugins/$plugin $fpath)
-
-                                if [[ -f "$BUNDLE/plugins/$plugin/$plugin.plugin.zsh" ]]; then
-                                        source $BUNDLE/plugins/$plugin/$plugin.plugin.zsh
-                                fi
-                        else
-                                zeesh_warning "$BUNDLE:t - $plugin isn't zeesh/oh-my-zshell compatable"
-                        fi
-                done
-                zeesh_message "$BUNDLE:t - plugins loaded"
-        else
-                zeesh_warning "$BUNDLE:t - no plugins found"
-        fi
+	zeesh_debug "loading plugins"
+	plugins=("${(ps: :)${1}}") 
+    
+	if [[ ${#plugins[@]} > 0 ]]; then
+		for plugin in $plugins; do
+		zeesh_debug "loading plugin $plugin"
+			if [[ -f "$BUNDLE/plugins/$plugin/$plugin.plugin.zsh" || -f "$BUNDLE/plugins/$plugin/_$plugin" ]]; then
+				fpath=($BUNDLE/plugins/$plugin $fpath)
+				if [[ -f "$BUNDLE/plugins/$plugin/$plugin.plugin.zsh" ]]; then
+					source $BUNDLE/plugins/$plugin/$plugin.plugin.zsh
+				fi
+			else
+				zeesh_warning "$BUNDLE:t - $plugin isn't zeesh/oh-my-zshell compatable"
+			fi
+		done
+			zeesh_message "$BUNDLE:t - plugins loaded"
+	else
+		zeesh_warning "$BUNDLE:t - no plugins found"
+	fi
 }
 
 # loads functions
 
 load_libs() {
-        zeesh_debug "loading libs"
-        libs=($BUNDLE/lib/*.zsh)
+	zeesh_debug "loading libs"
+	libs=($BUNDLE/lib/*.zsh)
 
-        if [[ ${#libs[@]} > 0 ]]; then
-                for lib in $libs; do
-                        zeesh_debug "loading lib $lib:t"
-                        source $lib
-                done
-                zeesh_message "$BUNDLE:t - libs loaded"
-        else
-                zeesh_warning "$BUNDLE:t - no libs found"
-        fi
+	if [[ ${#libs[@]} > 0 ]]; then
+		for lib in $libs; do
+			zeesh_debug "loading lib $lib:t"
+			source $lib
+		done
+			zeesh_message "$BUNDLE:t - libs loaded"
+	else
+		zeesh_warning "$BUNDLE:t - no libs found"
+	fi
 }
 
 #starts the bundler
@@ -82,20 +81,20 @@ zeesh_bundler() {
 		return 110
 	fi
 
-        if [[ ! -d "$BUNDLE/lib" ]]; then
-                zeesh_error "configs folder not found in bundle $BUNDLE:t (broken bundle?)"
-                return 111
-        fi
+	if [[ ! -d "$BUNDLE/lib" ]]; then
+		zeesh_error "configs folder not found in bundle $BUNDLE:t (broken bundle?)"
+		return 111
+	fi
 
-        if [[ ! -d "$BUNDLE/plugins" ]]; then
-                zeesh_error "plugins folder not found in bundle $BUNDLE:t (broken bundle?)"
-                return 112
-        fi
+	if [[ ! -d "$BUNDLE/plugins" ]]; then
+		zeesh_error "plugins folder not found in bundle $BUNDLE:t (broken bundle?)"
+		return 112
+	fi
 
-        if [[ ! -d "$BUNDLE/themes" ]]; then
-                zeesh_error "themes folder not found in bundle $BUNDLE:t (broken bundle?)"
-                return 113
-        fi
+	if [[ ! -d "$BUNDLE/themes" ]]; then
+		zeesh_error "themes folder not found in bundle $BUNDLE:t (broken bundle?)"
+		return 113
+	fi
 
 	zeesh_message "loading bundle $BUNDLE:t"
 
@@ -104,7 +103,7 @@ zeesh_bundler() {
 	load_libs
 
 	if [[ -z "$2" ]]; then
-                zeesh_warning "plugins from this bundle won't be loaded"     
+		zeesh_warning "plugins from this bundle won't be loaded"     
 	else
 		load_plugins $2
 	fi
