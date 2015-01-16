ff6712ba889e126606e5136491ee7fdc985e76bc
Karolis Lasys (aka. HyperWind) <karolioofficial@outlook.com>
2015-01-17 00:28:39 +0200

    added in the plugins folder with a testing plugin

---
+++ bundles/default/plugins/test_colors/test_colors.plugin.zsh
@@ -0,0 +1,11 @@
+
+test_colors() {
+	echo "$fg[black]test$reset_color"
+	echo "$fg[red]test$reset_color"
+	echo "$fg[green]test$reset_color"
+	echo "$fg[yellow]test$reset_color"
+	echo "$fg[blue]test$reset_color"
+	echo "$fg[magenta]test$reset_color"
+	echo "$fg[cyan]test$reset_color"
+	echo "$fg[white]test$reset_color"
+}
