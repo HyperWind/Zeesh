1cab83b8e8066f991d959a361c8ccc9a81692a73
Karolis Lasys (aka. HyperWind) <karolioofficial@outlook.com>
2015-01-19 19:28:33 +0200

    added in support for custom bundle init files

--- bundler.zsh
+++ bundler.zsh
@@ -12,13 +12,21 @@ Options:
     -b, --bundle    Bundle name.
     -t, --theme     Your desired theme.
     -p, --plugins   Your desired plugins.
-    -l, --libs      Loads libs form that bundle.
+    -c, --configs   Loads configs form that bundle.
 
 Other details:
     If you pass 'random' as an argument to the theme option, it'll select a random theme from that bundle.
-    The libs option doesn't take any arguments.
+    The configs option doesn't take any arguments.
     The plugins option takes a string as an argument.
     Give all as an argument in the plugins option to load everything."
+}
+
+# initializes a custom bundle
+
+load_custom_bundle() {
+	source $BUNDLE/init.zsh
+	init_custom_bundle $BUNDLE $1 $2 $3
+	unset -f init_custom_bundle
 }
 
 # loads themes
@@ -28,16 +36,16 @@ load_themes() {
 
 	if [[ "$1" == "random" ]]; then
 		zeesh_debug "using random theme"
-		themes=($BUNDLE/themes/*.zsh-theme)
+		themes=($BUNDLE/$2/*.zsh-theme)
 		local n=${#themes[@]}
 		n=$RANDOM%$n+1
 		local rand_theme=$themes[$n]
 		zeesh_message "$BUNDLE:t - loaded random theme $rand_theme"
 		source "$rand_theme"
 	else
-		if [[ ! -z $1 && -e "$BUNDLE/themes/$1.zsh-theme" ]]; then
+		if [[ ! -z $1 && -e "$BUNDLE/$2/$1.zsh-theme" ]]; then
 			zeesh_debug "file $1.zsh-theme found"
-			source "$BUNDLE/themes/$1.zsh-theme"
+			source "$BUNDLE/$2/$1.zsh-theme"
 			zeesh_message "$BUNDLE:t - loaded theme $1"
 		else
 			zeesh_warning "$BUNDLE:t - theme $1 not found."
@@ -53,17 +61,17 @@ load_plugins() {
 	if [[ "$1" != "all" ]]; then
 		plugins=("${(ps: :)${1}}") 
    	else
-		plugins=($BUNDLE/plugins/*)
+		plugins=($BUNDLE/$2/*)
 	fi
  
 	if [[ ${#plugins[@]} > 0 ]]; then
 		for plugin in $plugins; do
 		plugin="$plugin:t"
 		zeesh_debug "loading plugin $plugin"
-			if [[ -f "$BUNDLE/plugins/$plugin/$plugin.plugin.zsh" || -f "$BUNDLE/plugins/$plugin/_$plugin" ]]; then
-				fpath=($BUNDLE/plugins/$plugin $fpath)
-				if [[ -f "$BUNDLE/plugins/$plugin/$plugin.plugin.zsh" ]]; then
-					source "$BUNDLE/plugins/$plugin/$plugin.plugin.zsh"
+			if [[ -f "$BUNDLE/$2/$plugin/$plugin.plugin.zsh" || -f "$BUNDLE/$2/$plugin/_$plugin" ]]; then
+				fpath=($BUNDLE/$2/$plugin $fpath)
+				if [[ -f "$BUNDLE/$2/$plugin/$plugin.plugin.zsh" ]]; then
+					source "$BUNDLE/$2/$plugin/$plugin.plugin.zsh"
 				fi
 			else
 				zeesh_warning "$BUNDLE:t - $plugin isn't zeesh/oh-my-zshell compatable or wasn't found"
@@ -75,11 +83,11 @@ load_plugins() {
 	fi
 }
 
-# loads functions
+# loads configs
 
-load_libs() {
-	zeesh_debug "loading libs"
-	libs=($BUNDLE/lib/*.zsh)
+load_configs() {
+	zeesh_debug "loading configs"
+	libs=($BUNDLE/$1/*.zsh)
 
 	if [[ ${#libs[@]} > 0 ]]; then
 		for lib in $libs; do
@@ -98,10 +106,10 @@ zeesh_bundler() {
 	args=("$@")
 	zeesh_debug "$args[*]"
 
-	[[ -z "$args[*]" ]] && zeesh_error "zeesh_bundler -h for help"
-	[[ ! -z "$libs" ]] && libs=""
-    [[ ! -z "$plugins" ]] && plugins=""
-    [[ ! -z "$theme" ]] && theme=""
+	[[ -z "$args[*]" ]] && bundler_help
+	[[ -z "$libs" ]] || libs=""
+    [[ -z "$plugins" ]] || plugins=""
+    [[ -z "$theme" ]] || theme=""
 	
 	for ((n=1; n<${#args}+1; n++)); do
 		case $args[$n] in
@@ -123,8 +131,8 @@ zeesh_bundler() {
 			--plugins|-p)	# loads the plugins
 				plugins=$args[$n+1]
 				;;
-			--libs|-l)	# loads the libs
-				libs="true"
+			--configs|-c)	# loads the libs
+				configs="true"
 				;;
 		esac	
 	done
@@ -132,8 +140,13 @@ zeesh_bundler() {
 	zeesh_debug "$plugins"
 	zeesh_debug "$theme"
 
-	[[ ! -z "$libs" ]] && load_libs
-	[[ ! -z "$plugins" ]] && load_plugins $plugins
-	[[ ! -z "$theme" ]] && load_themes $theme
-	
+	if [[ -e "$BUNDLE/init.zsh" ]]; then
+		zeesh_debug "init at bundle $BUNDLE:t found"
+		load_custom_bundle $confgis $plugins $theme
+	else
+		zeesh_debug "using default settings"
+		[[ -z "$libs" ]] || load_configs configs
+		[[ -z "$plugins" ]] || load_plugins $plugins plugins
+		[[ -z "$theme" ]] || load_themes $theme themes
+	fi
 }
